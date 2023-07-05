2807ced6144bc951933bf0a88c4d88da87d33a01
lxhillwind <lxhillwind@foxmail.com>
2023-07-05 22:55:15 +0800

    remove unused code

--- lx.nim
+++
@@ -1,9 +0,0 @@
-proc `==`*(x: seq[uint8], y: string): bool =
-  if x.len != y.len:
-    return false
-  for i, ch in y.pairs:
-    if x[i] != ch.uint8:
-      return false
-  return true
-
-template `==`*(x: string, y: seq[uint8]): bool = y == x
